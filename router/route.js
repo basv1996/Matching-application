55ec74a62117b16b752444662aef7c18ecc10b2b
basv1996 <bas.vugts@gmail.com>
2021-02-12 11:00:56 +0100

    NPM install dotenv & Page title dynamic

--- router/route.js
+++ router/route.js
@@ -3,18 +3,24 @@ const router = express.Router();
 
 //Home Page route
 router.get('/', function(req, res){
-    res.render('pages/index');
+    res.render('pages/index', {
+        title: 'Home page'
+    });
 });
 
-//About Page route
+//Profile Page route
 router.get('/profile', function(req, res){
-    res.render('pages/profile');
+    res.render('pages/profile', {
+        title: 'Profile Page'
+    });
 });
 
 //route for 404 page
 router.get('*', function(req, res){
     //console.log('404');
-    res.render('pages/404');
+    res.render('pages/404', {
+        title: '404 Page'
+    });
 });
 //Source: https://stackoverflow.com/questions/11500204/how-can-i-get-express-js-to-404-only-on-missing-routes/11500298
 //res.render compiles my template
